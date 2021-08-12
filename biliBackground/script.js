ae98acad91c356159b150263880f5872c4488b7b
Jacken <wyg132456@163.com>
2021-08-12 16:51:11 +0800

    a

--- biliBackground/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-	var cards = document.getElementsByClassName("card");
-	console.log(cards);
-	for(var i=0; i<cards.length; i++){
-		cards[i].style.backgroundColor = "rgb(255,255,255,0.5)";
-	};
-});
