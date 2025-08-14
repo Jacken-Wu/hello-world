bea672156c28199b6b4dffdccd716323aadc1345
Jacken Desktop <wyg132456@163.com>
2025-08-14 19:51:19 +0800

    .

---
+++ biliBackground/bilibiliADBlock.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         bilibiliADBlock
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  删除bilibili首页广告
+// @author       -墓场-
+
+// @match        https://www.bilibili.com
+// @match        https://www.bilibili.com/?spm_id_from=*
+// @match        https://www.bilibili.com/v/popular/*
+
+// @icon         https://raw.githubusercontent.com/Jacken-Wu/hello-world/main/biliBackground/icon48.png
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    const checkAndRemove = () => {
+        document.querySelectorAll('div.feed-card').forEach(a => {
+            const b = a.querySelector('div.bili-video-card.is-rcmd');
+            if (b && !b.classList.contains('enable-no-interest')) {
+                console.log(a, 'removed');
+                a.remove();
+            }
+        });
+    };
+
+    checkAndRemove();
+
+    const observer = new MutationObserver(checkAndRemove);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
